f302c54037cc7fc36c7a9b9fd261f496ae164a77
Rovi Dela Rosa <Administrator@IVOR-MARGIELA>
2026-04-16 02:16:59 +0800

    New Process: Adding While-Loop, User Input & Input Validation

--- Dela_Rosa_Rovi_Andrie_ShoppingCartActivity/Dela_Rosa_Rovi_Andrie_ShoppingCartActivity/Product.cs
+++ Dela_Rosa_Rovi_Andrie_ShoppingCartActivity/Dela_Rosa_Rovi_Andrie_ShoppingCartActivity/Product.cs
@@ -6,17 +6,17 @@ namespace Dela_Rosa_Rovi_Andrie_ShoppingCartActivity
 {
     public class Product
     {
-        public int Id { get; set; }                   // I use automatic properties for automatically creating
-        public string Name { get; set; }              // Field and Encapsulation. Also, I put "public" (as my modifier)         
-        public double Price { get; set; }             // for me/you to access it in another class (which is the main class).   
+        public int Id { get; set; }                   
+        public string Name { get; set; }                   
+        public double Price { get; set; }             
         public int RemainingStock { get; set; }
 
         public void DisplayProduct()
         {
-            Console.WriteLine($"{Id}. {Name} - P{Price} (Stock: {RemainingStock})"); // Put these for menu and use string interpolation for cleaner
-        }                                                                             // arrangement of field.
+            Console.WriteLine($"{Id}. {Name} - P{Price} (Stock: {RemainingStock})"); 
+        }                                                                             
 
-        public bool HasEnoughStock(int quantity)     // Set bool as return type and add a return. 
+        public bool HasEnoughStock(int quantity)      
         {
             return RemainingStock >= quantity;
         }
--- Dela_Rosa_Rovi_Andrie_ShoppingCartActivity/Dela_Rosa_Rovi_Andrie_ShoppingCartActivity/Program.cs
+++ Dela_Rosa_Rovi_Andrie_ShoppingCartActivity/Dela_Rosa_Rovi_Andrie_ShoppingCartActivity/Program.cs
@@ -6,31 +6,54 @@ namespace ShoppingCartActivity
 {
     class Quiz
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("--- SHOPPING CART SYSTEM ---");
 
 
             Product[] menu = new Product[]
             {
-                new Product {Id = 1, Name = "Billabong Shorts", Price = 950.00, RemainingStock = 15},    // I use array initializer to easy add a new product
-                new Product {Id = 2, Name = "Nike Air Max", Price = 6500.00, RemainingStock = 10},       // and it's not limited by size.
+                new Product {Id = 1, Name = "Billabong Shorts", Price = 950.00, RemainingStock = 15},    
+                new Product {Id = 2, Name = "Nike Air Max", Price = 6500.00, RemainingStock = 10},      
                 new Product {Id = 3, Name = "Polo Plain Tee", Price = 650.00, RemainingStock = 25},
                 new Product {Id = 4, Name = "Adidas Crew Socks", Price = 299.00, RemainingStock = 30},
                 new Product {Id = 5, Name = "Nike Tank Top", Price = 199.00, RemainingStock = 9}
             };
 
-            Console.WriteLine("----------------------------");
-
-            Console.WriteLine("--- CLOTHING MENU ---");
-            Console.WriteLine("\nID | NAME | PRICE | STOCK");
-
-            foreach (Product bclothes in menu) // Foreach for looping the menu.
+            string choice = "Yes";
+            while (choice == "Yes")
             {
-                bclothes.DisplayProduct();
-            }
 
-            
+                Console.WriteLine("----------------------------");
+
+                Console.WriteLine("--- STORE MENU ---");
+                Console.WriteLine("\nID | NAME | PRICE | STOCK");
+
+                foreach (Product bclothes in menu)
+                {
+                    bclothes.DisplayProduct();
+                }
+
+                int order;
+                int quanti;
+
+                Console.Write("\nEnter product number (ID) : ");                                        // i create user input and use .TryParse for input validation
+                if (!int.TryParse(Console.ReadLine(), out order) || order < 1 || order > menu.Length)   // and convertion for these 2 new variable.
+                {
+                    Console.WriteLine("Invalid product number (ID)! Please enter the correct ID.");
+                    continue;
+                }
+
+                Console.Write("Enter Quantity: ");
+                if (!int.TryParse(Console.ReadLine(), out quanti) || quanti <= 0)
+                {
+                    Console.WriteLine("Invalid quatity! Try again.");
+                    continue;
+                }
+
+
+            }
+           
         }
     }
 }
