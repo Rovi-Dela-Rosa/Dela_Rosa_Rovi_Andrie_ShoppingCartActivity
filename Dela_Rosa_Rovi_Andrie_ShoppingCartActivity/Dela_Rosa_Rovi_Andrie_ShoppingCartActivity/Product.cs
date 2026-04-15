95c4eef5d985608ae0668715c822c77549926049
Rovi Dela Rosa <Administrator@IVOR-MARGIELA>
2026-04-15 19:40:41 +0800

    Refinement of Initial Process: Added Array of Objects & Menu Loop

--- Dela_Rosa_Rovi_Andrie_ShoppingCartActivity/Dela_Rosa_Rovi_Andrie_ShoppingCartActivity/Product.cs
+++ Dela_Rosa_Rovi_Andrie_ShoppingCartActivity/Dela_Rosa_Rovi_Andrie_ShoppingCartActivity/Product.cs
@@ -4,21 +4,22 @@ using System.Text;
 
 namespace Dela_Rosa_Rovi_Andrie_ShoppingCartActivity
 {
-    class Product
+    public class Product
     {
-        int Id;
-        string Name;
-        double Price;
-        int RemainingStock;
+        public int Id { get; set; }                   // I use automatic properties for automatically creating
+        public string Name { get; set; }              // Field and Encapsulation. Also, I put "public" (as my modifier)         
+        public double Price { get; set; }             // for me/you to access it in another class (which is the main class).   
+        public int RemainingStock { get; set; }
 
-        static void DisplayProduct()
+        public void DisplayProduct()
         {
+            Console.WriteLine($"{Id}. {Name} - P{Price} (Stock: {RemainingStock})"); // Put these for menu and use string interpolation for cleaner
+        }                                                                             // arrangement of field.
 
+        public bool HasEnoughStock(int quantity)     // Set bool as return type and add a return. 
+        {
+            return RemainingStock >= quantity;
         }
 
-        static void HasEnoughStock(int quantity)
-        {
-
-        }
     }
 }
--- Dela_Rosa_Rovi_Andrie_ShoppingCartActivity/Dela_Rosa_Rovi_Andrie_ShoppingCartActivity/Program.cs
+++ Dela_Rosa_Rovi_Andrie_ShoppingCartActivity/Dela_Rosa_Rovi_Andrie_ShoppingCartActivity/Program.cs
@@ -8,7 +8,29 @@ namespace ShoppingCartActivity
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("--- Shopping Cart System ---");
+            Console.WriteLine("--- SHOPPING CART SYSTEM ---");
+
+
+            Product[] menu = new Product[]
+            {
+                new Product {Id = 1, Name = "Billabong Shorts", Price = 950.00, RemainingStock = 15},    // I use array initializer to easy add a new product
+                new Product {Id = 2, Name = "Nike Air Max", Price = 6500.00, RemainingStock = 10},       // and it's not limited by size.
+                new Product {Id = 3, Name = "Polo Plain Tee", Price = 650.00, RemainingStock = 25},
+                new Product {Id = 4, Name = "Adidas Crew Socks", Price = 299.00, RemainingStock = 30},
+                new Product {Id = 5, Name = "Nike Tank Top", Price = 199.00, RemainingStock = 9}
+            };
+
+            Console.WriteLine("----------------------------");
+
+            Console.WriteLine("--- CLOTHING MENU ---");
+            Console.WriteLine("\nID | NAME | PRICE | STOCK");
+
+            foreach (Product bclothes in menu) // Foreach for looping the menu.
+            {
+                bclothes.DisplayProduct();
+            }
+
+            
         }
     }
 }
