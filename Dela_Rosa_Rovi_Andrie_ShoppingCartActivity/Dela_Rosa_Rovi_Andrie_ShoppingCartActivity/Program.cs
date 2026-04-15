27a2bbb747452e962bb5650bab425396097d164c
Rovi Dela Rosa <Administrator@IVOR-MARGIELA>
2026-04-16 04:58:39 +0800

    Final Phase of Buying Process: Addeng Bag Array, Sell Prevention & Method Access

--- Dela_Rosa_Rovi_Andrie_ShoppingCartActivity/Dela_Rosa_Rovi_Andrie_ShoppingCartActivity/Program.cs
+++ Dela_Rosa_Rovi_Andrie_ShoppingCartActivity/Dela_Rosa_Rovi_Andrie_ShoppingCartActivity/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Dela_Rosa_Rovi_Andrie_ShoppingCartActivity;
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 
 namespace ShoppingCartActivity
 {
@@ -11,7 +12,7 @@ namespace ShoppingCartActivity
             Console.WriteLine("--- SHOPPING CART SYSTEM ---");
 
 
-            Product[] menu = new Product[]
+            Product[] bmenu = new Product[]
             {
                 new Product {Id = 1, Name = "Billabong Shorts", Price = 950.00, RemainingStock = 15},    
                 new Product {Id = 2, Name = "Nike Air Max", Price = 6500.00, RemainingStock = 10},      
@@ -20,7 +21,12 @@ namespace ShoppingCartActivity
                 new Product {Id = 5, Name = "Nike Tank Top", Price = 199.00, RemainingStock = 9}
             };
 
-            string choice = "Yes";
+            Product[] bag = new Product[100];                              // i create new array of object (bag) using the former array to store an item with limited
+                                                                           // to 100 bags.
+            int bagCount = 0;                                              
+
+            var choice = "Yes";
+
             while (choice == "Yes")
             {
 
@@ -29,7 +35,7 @@ namespace ShoppingCartActivity
                 Console.WriteLine("--- STORE MENU ---");
                 Console.WriteLine("\nID | NAME | PRICE | STOCK");
 
-                foreach (Product bclothes in menu)
+                foreach (Product bclothes in bmenu)
                 {
                     bclothes.DisplayProduct();
                 }
@@ -37,8 +43,8 @@ namespace ShoppingCartActivity
                 int order;
                 int quanti;
 
-                Console.Write("\nEnter product number (ID) : ");                                        // i create user input and use .TryParse for input validation
-                if (!int.TryParse(Console.ReadLine(), out order) || order < 1 || order > menu.Length)   // and convertion for these 2 new variable.
+                Console.Write("\nEnter product number (ID) : ");                                        
+                if (!int.TryParse(Console.ReadLine(), out order) || order < 1 || order > bmenu.Length) 
                 {
                     Console.WriteLine("Invalid product number (ID)! Please enter the correct ID.");
                     continue;
@@ -47,9 +53,33 @@ namespace ShoppingCartActivity
                 Console.Write("Enter Quantity: ");
                 if (!int.TryParse(Console.ReadLine(), out quanti) || quanti <= 0)
                 {
-                    Console.WriteLine("Invalid quatity! Try again.");
+                    Console.WriteLine("Invalid quantity! Try again.");
                     continue;
                 }
+
+                Product picked = bmenu[order - 1];
+
+                if (picked.RemainingStock == 0)                                 
+                {
+                    Console.WriteLine("Apologies, product is out of stock.");          // invalidation of user to buy an out of stock product.
+                    continue;
+                }
+
+                if (!picked.HasEnoughStock(quanti))
+                {
+                    Console.WriteLine("Not enough stock for your purchase. Only " + picked.RemainingStock + " left in stock."); // accessing the method i created in class product, then 
+                    continue;                                                                                                   // preventing user to purchase an item greater than remaining
+                }                                                                                                               // stock.
+
+                bag[bagCount] = new Product { Id = picked.Id, Name = picked.Name, Price = picked.Price, RemainingStock = quanti };
+
+                picked.RemainingStock -= quanti;
+                bagCount++;
+                Console.WriteLine("The item was added to bag!");
+
+                Console.WriteLine("Do you want to purchase more item? (YES/NO)");
+                choice = Console.ReadLine().ToUpper();
+
 
 
             }
